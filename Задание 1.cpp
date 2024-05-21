0aedfdbf2fe90c9da1f32c668d6efc1bee6581bd
DanikAslamov <162562844+DanikAslamov@users.noreply.github.com>
2024-05-21 12:14:31 +0300

    Задание 1.cpp

--- Задание 1.cpp
+++ Задание 1.cpp
@@ -1,17 +1,19 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 /**
 * @breef - функция, производящая вычисление результата
 * param x - число х
 * param y - число y
+* @return - возвращает результат 
 */
-double getResultA(double x, double y);
+double getResultA(const double x, const double y);
 /**
 * @breef - функция, производящая вычисление результата
 * param x - число х
 * param y - число y
+* @return - возвращает результат
 */
-double getResultB(double x, double y);
+double getResultB(const double x, const double y);
 /**
 * @breef - главная функция программы
 * @return - возвращает 0, если функция выполнена верно, иначе 1.
@@ -25,12 +27,12 @@ int main()
     return 0;
 }
 
-double getResultA(double x, double y)
+double getResultA(const double x, const double y)
 {
     return (1 + x + (pow(x, 2) / 2) + (pow(x, 3) / 3) + (pow(x, 4) / 4));
 
 }
-double getResultB(double x, double y)
+double getResultB(const double x, const double y)
 {
     return (x * (sin(pow(x, 3)) + pow(cos(y), 2)));
 }
