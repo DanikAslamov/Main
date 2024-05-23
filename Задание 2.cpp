d1acd4df1245b76b475dd9346fc23a01469074c6
DanikAslamov <162562844+DanikAslamov@users.noreply.github.com>
2024-05-23 12:24:35 +0300

    Update Задание 2.cpp

--- Задание 2.cpp
+++ Задание 2.cpp
@@ -10,19 +10,19 @@ double getValue();
 * @breef Проверяет число на то, чтобы оно не было равно нулю или меньше нуля
 * @return Введённое значение
 */
-void checkValue(double value);
+void checkValue(const double value);
 /**
 * @breef Считает площадь поверхности шара
 * @param a - радиус шара
 * @return Площадь поверхности шара
 */
-double ballSurface(double a);
+double ballSurface(const double a);
 /**
 * @breef Считает объём шара
 * @param a - длина ребра шара
 * @return Объём шара
 */
-double ballVolume(double a);
+double ballVolume(const double a);
 /**
 * @breef - главная функция программы
 * @return - возвращает 0, если функция выполнена верно, иначе 1
@@ -47,18 +47,18 @@ double getValue()
     }
     return value;
 }
-void checkValue(double value)
+void checkValue(const double value)
 {
     if (value <= 0)
     {
         abort();
     }
 }
-double ballSurface(double a)
+double ballSurface(const double a)
 {
     return 4 * 3.14 * pow(a, 2);
 }
-double ballVolume(double a)
+double ballVolume(const double a)
 {
     return (4 / 3) * 3.14 * pow(a, 3);
 }
