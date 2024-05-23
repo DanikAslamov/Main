07b6ba698eb5c12f157606097c2a450f7d1846ac
DanikAslamov <162562844+DanikAslamov@users.noreply.github.com>
2024-05-23 18:58:05 +0300

    Update Задание 4.cpp

--- Задание 4.cpp
+++ Задание 4.cpp
@@ -12,19 +12,19 @@ double getNumber();
  * @param min - минимальное значение диапазона значений элементов массива
  * @param max - максимальное значение диапазона значений элементов массива
  */
-void checkRange(double min, double max);
+void checkRange(const double min, const double max);
 /**
 * @breef Табулирует функцию x(y) и выводит в виде столбца
 * @param minValue - минимальное значение диапазона
 * @param maxValue - максимальное значение диапазона
 * @param step - значение шага функции
 */
-void printTabFunction(double minValue, double maxValue, double step);
+void printTabFunction(const double minValue, const double maxValue, const double step);
 /**
 * @breef - Проверка правильности введённого шага
 * @param step - значение шага функции
 */
-void checkStep(double step);
+void checkStep(const double step);
 /**
 * @breef - главная функция программы
 * @return - возвращает 0, если функция выполнена верно, иначе 1.
@@ -39,6 +39,7 @@ int main()
     double step = getNumber();
     checkStep(step);
     printTabFunction(minValue, maxValue, step);
+    return 0;
 }
 double getNumber()
 {
@@ -51,7 +52,7 @@ double getNumber()
     }
     return number;
 }
-void checkRange(double min, double max)
+void checkRange(const double min, const double max)
 {
     if (min > max)
     {
@@ -59,7 +60,7 @@ void checkRange(double min, double max)
         abort();
     }
 }
-void printTabFunction(double minValue, double maxValue, double step)
+void printTabFunction(const double minValue, const double maxValue, const double step)
 {
     cout << setw(10) << "x" << setw(10) << "y" << endl;
     for (double i = minValue; i < maxValue + step; i = i + step) {
@@ -67,7 +68,7 @@ void printTabFunction(double minValue, double maxValue, double step)
         cout << setw(10) << i << setw(10) << a << endl;
     }
 }
-void checkStep(double step)
+void checkStep(const double step)
 {
     if (step <= 0)
     {
